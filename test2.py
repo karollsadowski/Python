f06b8fcc4e0d94dfe400d2cf54293268476bfa0b
karollsadowski <karoll.sadowski@yahoo.com>
2017-12-06 22:26:33 +0100

    First commit

--- test2.py
+++ test2.py
@@ -1,14 +1,16 @@
-import random
+import random, pyperclip
 r = random.randint(1,10)
 print(r)
 
+def hello(name):
+    print ('Hello ' + name,r - i)
 
-def hello():
-    print 'Hello', r - i
+def plusOne(nr):
+    return nr + 1
 
 i = 0
 while (i < r):
-    hello()
-    i = i + 1
+    hello('Bob')
+    i = plusOne(i)
 
-print "Koniec"
+print ('End')
